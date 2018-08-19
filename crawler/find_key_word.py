90e3c15d497580c851b2d712289449193074151c
RaymondMcGuire <raymondmg@foxmail.com>
2018-08-19 13:31:58 +0900

    key word analyse

--- crawler/find_key_word.py
+++ crawler/find_key_word.py
@@ -11,55 +11,67 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'crawler.settings'
 import django
 django.setup()
 from crawlerDataBase.models import Article
-import jieba
+import csv
 
-
-def export_txtfile(i,context):
-    name = './key_word_article/article_'+str(i)+'.txt'
+def export_txtfile(name,context):
+    name = './key_word_article/article/'+str(name)+'.txt'
     with open(name, 'w', encoding='utf-8') as f:
-        for c in range(len(context)):
-            f.write(context[c] + '\t')
+        f.write(context)
     f.close()
 
-def creadstoplist(stopwordspath):
-    stwlist = [line.strip()
-               for line in open(stopwordspath, 'r', encoding='utf-8').readlines()]
-    return stwlist
+def export_keywordfile(header,body):
+    name = './key_word_article/chk_keyword.csv'
+    with open(name, 'w', encoding='utf-8', newline='') as f:
+          writer = csv.writer(f)  
+          writer.writerow(header)
+          writer.writerows(body)
+          f.close()
 
-stwlist = creadstoplist('stopwords.txt')
-jieba.load_userdict("userdict.txt")
-
-def getArticleFromDataBase():
+def chkKeyWordFromDataBase(key_word_list):
     allData=Article.objects.all()
-    doc_set = [da.articleInfo for da in allData if da.articleInfo.strip()!=''] #and da.authorSex=='FeMale'
-    #doc_set = [da.articleInfo for da in allData if da.articleInfo.strip()!='' and da.authorPos=='北京']
-    return doc_set
-
-# compile sample documents into a list
-doc_set = getArticleFromDataBase()
-
-# list for tokenized documents in loop
-
-
-
-#是否控制读取数量
-maxProcessNum=500
-needConstraint=True
-
-index=0
-for i in doc_set:
-   
-    index+=1
+    header = ['ID','articleName','authorPos','authorSex','startTime','people','duringDay','cost','href']
+    header.extend(key_word_list)
+    body = []
     
-    if i.contains("琵琶湖"):
-        print(str(i))
-        export_txtfile(index,i)
-
+    idx = 0
+    for da in allData:
+        
+        if da.articleInfo.strip()!='':
+            article = str(da.articleInfo)
+            keywordContainOrNot = []
+            flag = False
+            for kw in key_word_list:
+                if kw in article:
+                    flag = True
+                    keywordContainOrNot.append(1)
+                else:
+                    keywordContainOrNot.append(0)
+            if flag:
+                cur_context = []
+                idx +=1
+                cur_context.append(idx)
+                cur_context.append(da.articleName)
+                cur_context.append(da.authorPos)
+                cur_context.append(da.authorSex)
+                cur_context.append(da.startTime)
+                cur_context.append(da.people)
+                cur_context.append(da.duringDay)
+                cur_context.append(da.cost)
+                
+                href = da.href
+                if da.href is None:
+                    href = "None"
+                
+                cur_context.append(href)
+                cur_context.extend(keywordContainOrNot)
+                body.append(cur_context)
+                export_txtfile(idx,da.articleInfo)
     
-    print('read article from database:'+str(index)+'/'+str(len(doc_set)))
-   
-    if index>=maxProcessNum and needConstraint:
-        break
+    export_keywordfile(header,body)
+
+            
+key_word_list = ["琵琶湖","输水"]
+chkKeyWordFromDataBase(key_word_list)
 
 
 
