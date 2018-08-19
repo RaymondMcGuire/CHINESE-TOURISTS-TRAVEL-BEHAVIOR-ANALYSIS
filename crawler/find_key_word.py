14d823ea4764b8973f329decd0902e8f3ad5790e
オウ　キョク <m02171017e@edu.teu.ac.jp>
2018-08-19 12:07:26 +0900

    key word search

--- crawler/find_key_word.py
+++ crawler/find_key_word.py
@@ -14,21 +14,8 @@ from crawlerDataBase.models import Article
 import jieba
 
 
-def splitResultFromLDAModel(data):
-    freq  =[]
-    words =[]
-    if '*' not in data or '+' not in data:
-        return freq,words
-    firArray=data.split('+')
-   
-    for d in firArray:
-        secArray=d.split('*')
-        freq.append(secArray[0])
-        words.append(secArray[1])
-    return freq,words
-
 def export_txtfile(i,context):
-    name = './export_article/article_'+str(i)+'.txt'
+    name = './key_word_article/article_'+str(i)+'.txt'
     with open(name, 'w', encoding='utf-8') as f:
         for c in range(len(context)):
             f.write(context[c] + '\t')
@@ -41,21 +28,6 @@ def creadstoplist(stopwordspath):
 
 stwlist = creadstoplist('stopwords.txt')
 jieba.load_userdict("userdict.txt")
-
-def dealWithCutWord(data,filterOrNot,flag):
-    curtexts = []
-    words=pseg.cut(data.strip())  
-    for w in words:
-     #remove stopwords
-     if w.word not in stwlist:
-      if len(w.word) > 1:
-        if filterOrNot:
-         flagStrArray=flag.split(',')
-         if w.flag in flagStrArray:
-            curtexts.append(w.word)
-        else:
-          curtexts.append(w.word)
-    return curtexts
 
 def getArticleFromDataBase():
     allData=Article.objects.all()
@@ -78,12 +50,10 @@ index=0
 for i in doc_set:
    
     index+=1
-   
-    #print(curdoc)
-   
-    #处理分词，True使用过滤分词/False不使用过滤
-    curdoc=dealWithCutWord(i,True,"pos,n")
-    export_txtfile(index,curdoc)
+    
+    if i.contains("琵琶湖"):
+        print(str(i))
+        export_txtfile(index,i)
 
     
     print('read article from database:'+str(index)+'/'+str(len(doc_set)))
